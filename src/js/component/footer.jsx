e3cb7e595afe1e137a5fac114e9e5f270c6aab73
billferrabone <billferrabone@gmail.com>
2022-08-20 01:08:31 +0000

    color footer

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 //create your first component
 const Footer = () => {
 	return (
-             <div className="card-footer text-muted">Copyright@Your website 2022</div>
+             <div className="card-footer text-muted bg-dark">Copyright@Your website 2022</div>
 
 	);
 };
